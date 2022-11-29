3d9f60ddfc25143ef2302d5bad94f4e5d8c71b47
Ruediger zu Dohna <snackbox@sinntr.eu>
2022-11-29 08:25:00 +0100

    add metric & try to configure tag

--- src/main/java/com/example/graphql/config/reload/reproducer/Hellos.java
+++ src/main/java/com/example/graphql/config/reload/reproducer/Hellos.java
@@ -3,12 +3,14 @@ package com.example.graphql.config.reload.reproducer;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
+import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import java.io.Closeable;
 
 @Path("/")
 @GraphQLApi
@@ -19,13 +21,14 @@ public class Hellos {
     @Inject Api api;
 
     @Query
+    @Counted
     public String hello() {throw new FailedHelloException(failedHelloMessage);}
 
     @GET @Path("/indirect")
     public String indirect() {return api.hi("Jane");}
 
     @RegisterRestClient(configKey = "api")
-    public interface Api {
+    public interface Api extends Closeable {
         @GET @Path("/hi")
         String hi(@QueryParam("whom") String whom);
     }
